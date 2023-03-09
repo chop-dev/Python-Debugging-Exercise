dba44928c1d4b4498a1024ba94de44d1598d8e88
chop-dev <chopy246@gmail.com>
2023-03-09 19:35:56 +0200

    Solution For Debug Exercise

--- broken.py
+++ broken.py
@@ -1,42 +1,50 @@
 def quick_sort(numbers):
-   quick_sort_helper(numbers, 0, len(numbers)-1)
+    quick_sort_helper(numbers, 0, len(numbers)-1)
 
 def quick_sort_helper(numbers, first, last):
-   if first<last:
+    if first<last:
 
-       splitpoint = partition(numbers, first, last)
+        splitpoint = partition(numbers, first, last)
 
-       quick_sort_helper(numbers, first, splitpoint-1)
-       quick_sort_helper(numbers, splitpoint+1, last)
+        quick_sort_helper(numbers, first, splitpoint-1)
+        quick_sort_helper(numbers, splitpoint+1, last)
 
 
 def partition(numbers, first, last):
-   pivotvalue = numbers[first]
+    pivotvalue = numbers[first]
 
-   leftmark = first+1
-   rightmark = last
+    leftmark = first+1
+    rightmark = last
 
-   done = False
-   while not done:
+    done = False
+    while not done:
+        
+        while leftmark <= rightmark and numbers[leftmark] <= pivotvalue:
+            
+            # Ensure the loop will end when leftmark reaches a index greater than the pivot value
+            # Changes: Incremented leftmark
+            leftmark += 1
 
-       while leftmark <= rightmark and numbers[leftmark] <= pivotvalue:
-           leftmark = rightmark + 1
+        while numbers[rightmark] >= pivotvalue and rightmark >= leftmark:
+            
+            # Ensure the loop will end when rightmark reaches a index smaller than the pivot value
+            # Changes: Decrement rightmark
+            rightmark -= 1
+        
+        # Ensure the loop is not infinite
+        # Changes: Removed -1 after leftmark 
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = numbers[leftmark]
+            numbers[leftmark] = numbers[rightmark]
+            numbers[rightmark] = temp
 
-       while numbers[rightmark] >= pivotvalue and rightmark >= leftmark:
-           rightmark = leftmark -1
+    temp = numbers[first]
+    numbers[first] = numbers[rightmark]
+    numbers[rightmark] = temp
 
-       if rightmark < leftmark -1:
-           done = True
-       else:
-           temp = numbers[leftmark]
-           numbers[leftmark] = numbers[rightmark]
-           numbers[rightmark] = temp
-
-   temp = numbers[first]
-   numbers[first] = numbers[rightmark]
-   numbers[rightmark] = temp
-
-   return rightmark
+    return rightmark
 
 numbers = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 print("Before Sort: {}".format(numbers))
